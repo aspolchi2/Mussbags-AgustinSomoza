fdbdb48996ece38df4b350f401de2e83db3dce89
aspolchi <aspolchi@gmail.com>
2022-03-08 18:36:11 -0300

    asd

--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
             <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link" aria-current="page" href="#">Home</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">Bolsos</a>
