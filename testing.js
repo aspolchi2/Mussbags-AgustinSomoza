ad4f8f5cc7d36c13e450b042fc5bfb61c2bf8d9f
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-03-08 01:07:38 -0300

    asd

--- testing.js
+++ testing.js
@@ -1,8 +1,36 @@
 const object = ['ford', 'toyota', 'honda', 'chevrolet']
 
 
-function test(){
+function test() {
     console.log(...object);
 }
 
 test()
+
+
+let id = Math.random()// genera numero random
+const persona = {
+    name: 'jhon',
+    ['baz' + id]: 40,// id pasa como una variable
+
+};
+
+function pers() {
+    console.log(persona);
+}
+pers()
+
+let { a: val } = { a: 2 };
+console.log(val);
+
+//asignar variables a un array
+let [a, b, c, d] = [1, 2, 3, 4]
+
+console.log(a);
+
+
+//si la variable no tiene valor asignado le asigna el primer valor dado
+let [z = 1, x = 3, v = 4] = [1, 2]
+
+console.log(v);
+
