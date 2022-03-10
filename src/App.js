6b5795e022b40378c250fa93db33eceb0d463209
aspolchi <aspolchi@gmail.com>
2022-03-10 19:19:44 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Navbar from './components/header/navbar/Navbar';
+import ItemListContainer from './components/main/Main';
 
 
 
 function App() {
   return (
-    <header className="header">
-      <Navbar />
-    </header>
+    <body>
+      <header className="header">
+        <Navbar />
+      </header>
+      <main>
+       <ItemListContainer />
+      </main>
+    </body>
   );
 }
 
---
+++ src/components/main/Main.js
@@ -0,0 +1,14 @@
+const ItemListContainer = () =>{
+    return (
+        <div>
+        <ul>
+            <li>Accesorios</li>
+            <li>Bolsos</li>
+            <li>Carteras</li>
+        </ul>
+
+        </div>
+    )
+}
+
+export default ItemListContainer
