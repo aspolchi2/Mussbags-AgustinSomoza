088bdea1f6ecc80bdb236b6d2c7ff0284cd4de7b
aspolchi <aspolchi@gmail.com>
2022-03-10 17:38:14 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/header/navbar/Navbar';
-import CartLogo from './components/header/CartLogo/CartLogo';
 
 
 
 function App() {
   return (
     <header className="header">
-      <Navbar/ >
-      
+      <Navbar />
     </header>
   );
 }
