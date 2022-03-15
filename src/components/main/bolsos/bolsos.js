401cb2b2990c46fd64a58b20b7fba63ebe84f84b
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-03-14 22:36:18 -0300

    Update bolsos.js

--- src/components/main/bolsos/bolsos.js
+++ src/components/main/bolsos/bolsos.js
@@ -1,15 +1,34 @@
+import React, {useState, useEffect} from "react"
+
+
 
 
 const Bags = (prop) => {
-    return (
-        <article className='bags col-12 col-sm-6 col-md-4 col-lg-3'>
+ const [count, setCount] = useState(0)
+ console.log('aqui seria el...');
+ useEffect(()=> {
+     console.log('App mounted');
+     return () =>{
+         console.log('will unmount');
+     }
+    
+ }, [count])
+
+     return (
+        <article className='bags col-12 col-sm-6 col-md-4 col-lg-3' >
             <main className='bags__img'>
                 <img className='img-fluid' src={prop.src} alt='Imagen de un bolso' ></img>
             </main>
             <footer className='bags__footer'>
                 <p className='bags__footer__p'>{prop.text}</p>
+               <button onClick={()=> setCount(count + 1)}>
+                   Clickeame
+               </button>
+               <p>me clickeaste {count} ultima vez a las {Date()} </p>
+               
             </footer>
         </article>
+        
     )
 }
 export default Bags
