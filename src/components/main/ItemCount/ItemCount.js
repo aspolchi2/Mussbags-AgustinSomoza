1cd11f4ae65e9ba889b97db99be3e164ab9ed8c2
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-03-18 21:39:37 -0300

    Arreglos
    se hizo una funcion para sumar y otra para restar.
    Se agrego useRef()
    y la funcion AddToCart no se ejecuta cuando el stock o el contador es 0

--- src/components/main/ItemCount/ItemCount.js
+++ src/components/main/ItemCount/ItemCount.js
@@ -1,48 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-import {useState}  from "react"
+import { useState } from "react"
 
-const ItemCount = ({stock, initial, OnAdd}) => {
+const ItemCount = ({ stock, initial, OnAdd }) => {
     //creamos hook 'useState'
     const [count, setCount] = useState(initial)
-    const Counter = (value) => {
-        document.querySelector('.spanCount').classList.remove('wrong')
-        //verificamos el valor de setCount 
-        if (value === 'add') {
-            setCount(count + 1)
-            if (count >= stock) {
-                //agrega una clase a 'spanCount' cuando 'count' sea igual o mayor al stock que no funciona por que me selecciona el primer div con 'spanCount'
-                document.querySelector('.spanCount').classList.add('wrong')
-                
-                setTimeout(function(){
-                    document.querySelector('.spanCount').classList.remove('wrong')
-                }, 1000)
-                //Vuelve el valor de 'count' al maximo de stock
-                setCount(stock)
-            }
-        } else {
-            //si el valor que recibe "Counter" no es 'add' descuenta 1
-            setCount(count - 1)
-            //si el valor de 'count' es menor o igual al valor de 'initial' setea 'count' al valor de 'initial' 
-            if (count <= initial) {
-                setCount(initial)
-            }
+    //useRef es como el $().this en jquery se usa para seleccionar solamente el elemento que se modifica
+    const MaxStock = useRef()
+
+    const More = () => {
+        setCount(count + 1)
+        if (count >= stock) {
+            //agrega una clase a 'spanCount' cuando 'count' sea igual o mayor al stock que no funciona por que me selecciona el primer div con 'spanCount'
+            MaxStock.current.classList.add('wrong')
+            setTimeout(function () {
+                MaxStock.current.classList.remove('wrong')
+            }, 1000)
+            //Vuelve el valor de 'count' al maximo de stock
+            setCount(stock)
         }
-    
-
-
     }
-    const AddToCart = ()=>{
-        const StockLeft = stock - count
-       OnAdd(count, StockLeft)
 
+    const Less = () => {
+        setCount(count - 1)
+        //si el valor de 'count' es menor o igual al valor de 'initial' setea 'count' al valor de 'initial' 
+        if (count <= initial) {
+            setCount(initial)
+        }
     }
-   
+    const AddToCart = () => {
+    if (stock > 0 && count > 0){
+            const StockLeft = stock - count
+            OnAdd(count, StockLeft)
+
+        }
+    }
+
+
+
     return (
         <div className="ItemCount">
-            <button className='counter' onClick={() => Counter('add')}>+</button>
-            <span className="spanCount">{count}</span>
-            <button className='counter' onClick={() => Counter()}>-</button>
+            <button className='counter' onClick={() => More()}>+</button>
+            <span ref={MaxStock} className="spanCount">{count}</span>
+            <button className='counter' onClick={() => Less()}>-</button>
             <button className="counter" onClick={() => AddToCart()}>Buy</button>
         </div>
     )
